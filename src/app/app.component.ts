d148b46966c92cc44c754ffdd83dca7b8253ce36
ehsan <ehsan@gmail.com>
2018-11-29 01:14:54 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+interface IContact{
+  id:number;
+  name:string;
+  family:string;
+  phone:number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CRUD';
+
+  contacts : IContact[] = [];
+  contact : IContact;
+
+  constructor(){
+    this.clear();
+  }
+
+  clear(){
+    this.contact = {
+      id: null,
+      name : null,
+      family : null,
+      phone : null
+    }
+  }
+
+  add(){
+    if(!this.contact.id){
+      const lastId = this.contacts.length ? Math.max(...this.contacts.map(f=>+f.id)) : -1;
+      this.contact.id = lastId + 1;
+      this.contacts.push({...this.contact});
+      this.clear();
+    } else {
+      this.contacts[this.contacts.findIndex(f=>f.id === this.contact.id)] = this.contact;
+      this.clear();
+    }
+  }
+
+  remove(id){
+    this.contacts = this.contacts.filter(f=>f.id !== id);
+  }
+
+  edit(item : IContact){
+    this.contact = item;
+  }
+
+
+
+
 }
